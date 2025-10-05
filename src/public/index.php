d1c351af51ca495b2d2be47933ca4a2eb9228e68
JesusLugo2002 <jesusdlugo02@gmail.com>
2025-10-05 15:21:51 +0100

    Add style to the page

--- src/public/index.php
+++ src/public/index.php
@@ -188,63 +188,63 @@ class Renderer {
     static public function ascii(int $attemptsLeft): string {
         $status = [
         6 => " 
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-========= ",
+                                                            +---+                                           .-.
+                                                            |   |                                            ) )
+                                                                |                                           '-'
+                                                                |
+        ((_))                                                   |
+        (o o)                                                   |
+=========\_/===============================================================================================================",
         5 => " 
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-========= ",
+                                                            +---+                                           .-.
+                                                            |   |                                            ) )
+                                                            O   |                                           '-'
+                                                                |
+        ((_))                                                   |
+        (o o)                                                   |
+=========\_/===============================================================================================================",
         4 => " 
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-========= ",
+                                                            +---+                                           .-.
+                                                            |   |                                            ) )
+                                                            O   |                                           '-'
+                                                            |   |
+        ((_))                                                   |
+        (o o)                                                   |
+=========\_/===============================================================================================================",
         3 => " 
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-========= ",
+                                                            +---+                                           .-.
+                                                            |   |                                            ) )
+                                                            O   |                                           '-'
+                                                           /|   |
+        ((_))                                                   |
+        (o o)                                                   |
+=========\_/===============================================================================================================",
         2 => " 
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-========= ",
+                                                            +---+                                           .-.
+                                                            |   |                                            ) )
+                                                            O   |                                           '-'
+                                                           /|\  |
+        ((_))                                                   |
+        (o o)                                                   |
+=========\_/===============================================================================================================",
         1 => " 
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-========= ",
+                                                            +---+                                           .-.
+                                                            |   |                                            ) )
+                                                            O   |                                           '-'
+                                                           /|\  |
+        ((_))                                              /    |
+        (o o)                                                   |
+=========\_/===============================================================================================================",
         0 => " 
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-========= "
+                                                            +---+                                           .-.
+                                                            |   |                                            ) )
+                                                            O   |                                           '-'
+                                                           /|\  |
+        ((_))                                              / \  |
+        (o o)                                                   |
+=========\_/==============================================================================================================="
     ];
-        return "<pre>" . $status[$attemptsLeft] . "</pre>";
+        return "<pre class='row justify-content-center'>" . $status[$attemptsLeft] . "</pre>";
     }
 }
 
@@ -262,9 +262,10 @@ $word = $game->getWord();
 $message = "";
 
 if ($game->isWon()) {
-    $message = "Felicidades ¡Ganaste! La palabra era: $word"; 
+    $message = "<p class='text-success display-5 text-center'>Felicidades, ¡Has ganado!</p>"; 
 } else if ($game->isLost()) {
-    $message = "Lo siento ¡Perdiste! La palabra era: $word";
+    $message = "<p class='text-danger display-5 text-center'>Lo siento, ¡Has perdido!</p>
+    <p class='text-danger fs-1 text-center'>La palabra era <span class='fw-bold'>$word</span>.</p>";
 }
 
 $storage->set("state", $game->toState());
@@ -274,7 +275,10 @@ $storage->set("state", $game->toState());
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Ahorcado en PHP</title>
+    <meta name="author" content="Jesus Lugo">
+    <title>El Ahorcado - Jesús Lugo</title>
+    <link rel="shortcut icon" href="./img/icon.svg" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <script>
     window.onload = function() {
@@ -288,24 +292,58 @@ $storage->set("state", $game->toState());
     };
 </script>
 <body>
-<h1>Juego del Ahorcado</h1>
+    <main class="container d-flex justify-content-center align-items-middle flex-column">
+        <pre class="row justify-content-center mt-5">
 
-<?php echo Renderer::ascii($game->getAttemptsLeft()); ?>
+██████████ ████       █████████   █████                                                █████         
+░░███░░░░░█░░███      ███░░░░░███ ░░███                                                ░░███          
+ ░███  █ ░  ░███     ░███    ░███  ░███████    ██████  ████████   ██████   ██████    ███████   ██████ 
+ ░██████    ░███     ░███████████  ░███░░███  ███░░███░░███░░███ ███░░███ ░░░░░███  ███░░███  ███░░███
+ ░███░░█    ░███     ░███░░░░░███  ░███ ░███ ░███ ░███ ░███ ░░░ ░███ ░░░   ███████ ░███ ░███ ░███ ░███
+ ░███ ░   █ ░███     ░███    ░███  ░███ ░███ ░███ ░███ ░███     ░███  ███ ███░░███ ░███ ░███ ░███ ░███
+ ██████████ █████    █████   █████ ████ █████░░██████  █████    ░░██████ ░░████████░░████████░░██████ 
+░░░░░░░░░░ ░░░░░    ░░░░░   ░░░░░ ░░░░ ░░░░░  ░░░░░░  ░░░░░      ░░░░░░   ░░░░░░░░  ░░░░░░░░  ░░░░░░  
+                                                                                                      
+                                                                                                      
 
-<p>Palabra: <?php echo implode(" ", str_split($game->getMaskedWord())); ?></p>
-<p>Intentos restantes: <?php echo $game->getAttemptsLeft(); ?></p>
-<p>Letras usadas: <?php echo implode(", ", $game->getUsedLetters()); ?></p>
+        </pre>
+        <?php echo Renderer::ascii($game->getAttemptsLeft()); ?>
 
-<?php if (!$message): ?>
-    <form method="post">
-        <label>Introduce una letra:</label>
-        <input type="text" name="letter" maxlength="1" required>
-        <button type="submit">Adivinar</button>
-    </form>
-<?php else: ?>
-    <p><strong><?php echo $message; ?></strong></p>
-    <a href="reset.php">Jugar de nuevo</a>
-<?php endif; ?>
-
+        <div class="row">
+            <div class="col">
+                <div class="row text-center">
+                    <p class="display-5">Adivina la palabra</p>
+                    <p class="display-5 fw-bold"><?php echo implode(" ", str_split($game->getMaskedWord())); ?></p>
+                </div>
+                <div class="row mt-3 text-center">
+                    <p class="fs-4">Intentos restantes → <?php echo $game->getAttemptsLeft(); ?></p>
+                    <p class="fs-4">Letras usadas → <?php echo implode(", ", $game->getUsedLetters()); ?></p>
+                </div>
+            </div>
+            <div class="col">
+                <div class="row">
+                    <?php if (!$message): ?>
+                        <form class="border-0 p-0 d-flex justify-content-center flex-column align-items-middle" method="post">
+                            <label class="display-5 text-center m-0 p-0">Introduce una letra</label>
+                            <div class="input-group mt-4">
+                                <span class="input-group-text" id="basic-addon1">A-Z</span>
+                                <input class="form-control form-control-lg" type="text" name="letter" maxlength="1" required>
+                            </div>
+                            <button class="btn btn-outline-dark mt-1 px-5" type="submit">Adivinar</button>
+                        </form>
+                    <?php else: ?>
+                        <?php echo $message; ?>
+                        <a class="btn btn-outline-dark" href="reset.php">Jugar de nuevo</a>
+                    <?php endif; ?>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="alert alert-primary text-center mt-5" role="alert">
+                No se ha lastimado ningún muñequito de palos en la creación de este juego.
+            </div>
+        </div>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
