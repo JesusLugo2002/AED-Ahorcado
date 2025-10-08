d1151ee1b980ecd0b4a4cfa0442752020779a486
JesusLugo2002 <jesusdlugo02@gmail.com>
2025-10-08 23:43:24 +0100

    Add files in data/; add confifig.php; add Autoloader.php

---
+++ ahorcado/config/config.php
@@ -0,0 +1,11 @@
+<?php 
+
+return [
+    'game' => [
+        'max_attempts' => 6
+    ],
+    'storage' => [
+        'words_file' => __DIR__ . '../data/words.json',
+        'games_file' => __DIR__ . '../data/games.json'
+    ]
+];
