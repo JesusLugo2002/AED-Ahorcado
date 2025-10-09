53fbd37c5eaa09fa5fdcb2ccbfb409bb79866d92
JesusLugo2002 <jesusdlugo02@gmail.com>
2025-10-09 12:48:47 +0100

    Add basic get/post form for testing

--- ahorcado/public/index.php
+++ ahorcado/public/index.php
@@ -9,7 +9,11 @@ use App\Domain\Entity\Game as Game;
 $wordProvider = new WordProvider($config['storage']['words_file']);
 $game = new Game($wordProvider->getRandomWord(), $config['game']['max_attempts']);
 
-echo $game;
+if (isset($_POST['letter'])) {
+    $message = $_POST['letter'];
+} else {
+    $message = "No hay letra";
+}
 ?>
 
 <!DOCTYPE html>
@@ -17,8 +21,22 @@ echo $game;
 <head>
     <meta charset="UTF-8">
     <meta name="author" content="Jesus Lugo">
-    <title>Ahorcado en PHP</title>
+    <title>Ahorcado en PHP</title>  
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
+    <main class="container mt-5">
+        <p><?php echo $game ?></p>
+        <div class="row">
+            <form method="post" class="col text-center">
+                <label for="letter">Letra: </label>
+                <input type="text" name="letter" id="letter" maxlength="1" required>
+                <input type="submit" class="btn btn-outline-success" value="Adivinar">
+            </form>
+            <p class="col text-center"><?php echo $message ?></p>
+            <a href="." class="col btn btn-outline-primary">Reiniciar juego</a>
+        </div>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
